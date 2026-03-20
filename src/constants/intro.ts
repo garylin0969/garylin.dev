9e861b3860050bc9e5842e6239b0b251d3eee349
GaryLin <garylin2@anuegroup.com.tw>
2026-03-20 09:58:34 +0800

    docs: revise introductory content

--- src/constants/intro.ts
+++ src/constants/intro.ts
@@ -12,5 +12,5 @@ export const HOME_INTRO_LIST = ['A Frontend Developer'];
  */
 export const ABOUT_INTRO_LIST = [
     'Hi there',
-    '我是 Gary Lin，目前於 鉅亨網 擔任前端工程師，主要使用 Next.js、React.js、TypeScript 和 Tailwind CSS。閒暇時喜愛看動漫、小說，經常在社群平台上衝浪的哥布林。',
+    '我是 Gary Lin，是名前端工程師，專注於 Next.js 與 React.js 生態系，並習慣以 TypeScript 與 Tailwind CSS 打造網頁。除了程式設計之外，我也喜歡動漫、小說和影劇，同時也是位經常在社群平台上衝浪的哥布林。',
 ];
