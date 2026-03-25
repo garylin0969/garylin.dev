d513b02fad8d3a78e007607adff0fbf00504e728
GaryLin <garylin2@anuegroup.com.tw>
2026-03-25 17:04:32 +0800

    refactor: Replace custom `BaseImage` component with `next/image` and enable Next.js Image optimization, React Compiler, and Cache Components

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,17 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     /* config options here */
     reactCompiler: true,
+    // 啟用 Cache Components
+    cacheComponents: true,
     logging: {
         browserToTerminal: true,
         // 'error' — errors only (default)
         // 'warn'  — warnings and errors
         // true    — all console output
         // false   — disabled
+    },
+    images: {
+        unoptimized: true,
     },
 };
 
