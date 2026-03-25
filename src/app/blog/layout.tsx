d46a70921e390527793be7ce528362f1131511ff
GaryLin <garylin2@anuegroup.com.tw>
2026-03-25 17:23:39 +0800

    feat: update blog layout structure and content

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,10 +1,4 @@
 import { ReactNode } from 'react';
-import AuthorCard from '@/components/molecules/author-card';
-import BlogCategoriesCard from '@/components/molecules/blog-categories-card';
-import BlogStatsCard from '@/components/molecules/blog-stats-card';
-
-// import GithubStatsCard from '@/components/molecules/github-stats-card';
-// import { DEFAULT_TOP_LANGS_CONFIG, DEFAULT_GITHUB_STATS_PARAMS } from '@/constants/github-stats';
 
 /**
  * 部落格佈局元件的屬性介面。
@@ -23,35 +17,18 @@ interface BlogLayoutProps {
  */
 const BlogLayout = ({ children }: BlogLayoutProps) => {
     return (
-        <div className="container flex flex-col lg:flex-row">
-            <aside className="hidden w-74 space-y-4 lg:block">
-                {/* 作者 */}
-                <AuthorCard />
-                {/* 文章統計 */}
-                <BlogStatsCard />
-                {/* 文章分類 */}
-                <BlogCategoriesCard />
-                {/* GitHub 使用最多的程式語言 */}
-                {/* <GithubStatsCard
-                    type="top-langs"
-                    params={DEFAULT_TOP_LANGS_CONFIG}
-                    width={296}
-                    height={282}
-                    loading="eager"
-                    alt="top languages"
-                /> */}
-                {/* GitHub 統計 */}
-                {/* <GithubStatsCard
-                    type="stats"
-                    params={DEFAULT_GITHUB_STATS_PARAMS}
-                    width={296}
-                    height={190}
-                    loading="eager"
-                    alt="github stats"
-                /> */}
-            </aside>
-            <div className="flex-1">{children}</div>
-        </div>
+        // <div className="container flex flex-col lg:flex-row">
+        //     <aside className="hidden w-74 space-y-4 lg:block">
+        //         {/* 作者 */}
+        //         <AuthorCard />
+        //         {/* 文章統計 */}
+        //         <BlogStatsCard />
+        //         {/* 文章分類 */}
+        //         <BlogCategoriesCard />
+        //     </aside>
+        //     <div className="flex-1">{children}</div>
+        // </div>
+        <div className="container">{children}</div>
     );
 };
 
