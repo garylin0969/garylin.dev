d55fd5f79717815cf0ff5b21034bc888020f9d02
GaryLin <garylin2@anuegroup.com.tw>
2026-03-26 10:59:09 +0800

    refactor: Simplify footer copyright display logic and update link title

--- src/components/organisms/footer/footer.tsx
+++ src/components/organisms/footer/footer.tsx
@@ -1,19 +1,15 @@
 import { FOOTER_CONFIG } from '@/constants/footer';
 
-const currentYear = new Date().getFullYear();
-
 /**
  * 網站頁尾元件。
  *
  * 顯示版權資訊、作者連結和相關年份。
- * 年份會根據當前時間自動更新。
  */
 const Footer = () => {
     return (
         <footer className="py-4 text-center text-sm tracking-wide">
             <p>
-                &copy;{' '}
-                {FOOTER_CONFIG.startYear === currentYear ? currentYear : `${FOOTER_CONFIG.startYear}-${currentYear}`}
+                {FOOTER_CONFIG.startYear} - PRESENT &copy;{' '}
                 <span className="mx-1">
                     <a
                         className="hover:text-primary"
@@ -24,7 +20,6 @@ const Footer = () => {
                         {FOOTER_CONFIG.linkTitle}
                     </a>
                 </span>
-                {FOOTER_CONFIG.copyright}
             </p>
         </footer>
     );
